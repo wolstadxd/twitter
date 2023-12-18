3ddbc231f13ce8f73ecbe8e12364c17655fe6ea4
Pavel Wolski <pavelwolski@MacBook-Air-Pavel.local>
2023-12-18 21:51:11 +0200

    first upload

---
+++ src/components/SignUp/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './signup.scss'
+
+
+const SignUp = () => {
+    return (
+        <>
+                <div className='header_signup'>
+                    <img src="images/twitter_logo.png" alt="" />
+                    <h1 className='signup_title'>Create an account</h1>
+                    
+                </div>
+        </>
+    )
+}
+
+export default SignUp
