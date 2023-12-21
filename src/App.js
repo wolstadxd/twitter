c9620820414027348a36c7067ed0a8ff9f4d92c8
Pavel Wolski <pavelwolski@MacBook-Air-Pavel.local>
2023-12-21 05:05:40 +0200

    forms_console_log

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Login from './components/Login';
 function App() {
   return (
     <div className="wrapper">
+        <SignUp/>
         <Login/>
-        {/* <SignUp/> */}
     </div>
   );
 }
